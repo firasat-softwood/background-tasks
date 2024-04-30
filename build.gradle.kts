590531b864bf03d91c532ddfcf1e12263f82b73a
Firasat Noor <150341556+firasat-softwood@users.noreply.github.com>
2024-04-30 14:22:08 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,13 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.play.services.location)
+    implementation(libs.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.play.services.location)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit.v111)
+    androidTestImplementation(libs.espresso.core.v320)
 }
